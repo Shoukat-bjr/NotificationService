322011dbd4d8cbb2e9c876c7984b436be4315461
Shoukat Mirza <shoukat.bjr@gmail.com>
2024-03-21 16:58:19 +0500

    initial commit

---
+++ src/app/Jobs/UserCreatedJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+use Illuminate\Support\Facades\Log;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UserCreatedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info('User created:', [
+            'user_id' => $this->data['id'],
+            'username' => $this->data['name'],
+            'email' => $this->data['email'],
+        ]);
+    }
+}
